c0efe24a99a97c50a47bca30d2d42e00a2e25fc7
Aruna Jayasena <aruna.15@cse.mrt.ac.lk>
2020-10-14 00:49:04 +0530

    tool tested

---
+++ v2g.py
@@ -0,0 +1,86 @@
+import re
+import networkx as nx
+import matplotlib.pyplot as plt
+
+f = open("src_verilog/lib/flit_buffer.v", "r")
+module_name="module flit_buffer"
+en=False
+variables=[]
+variables2=[]
+matches = ["input", "output","wire","reg"]
+body=[]
+
+for x in f:
+    if x[0:1]!="//":
+        if module_name in x:
+            en=True
+        if en:
+            if any(y in x for y in matches):
+                temp=(x.split("//")[0]).split( )
+                if '];'in x:
+                    strt=x.split("[")[-2]
+                    ptr=strt.index("]")
+                    strt=strt[ptr+1:].replace(" ", '')
+                    # print(strt)
+                    variables.append(strt)
+                else: variables.append(str([re.split(',',(a.split(";")[0])) for a in temp if ";" in a])[2:-2])
+            else: body.append(x)
+            if "endmodule" in x:
+                break
+for m in variables:
+    variables2.extend((m.replace("'", '')).split(","))
+print((variables2))
+variables2 = [i for i in variables2 if i] 
+
+edges_set=[]
+
+# for x in variables2:
+#     for y in body:
+#         if x in y:
+#             if "assign" in y:
+#                 temp1=y.replace("assign", '')
+#                 edges_set.append(set(temp1.split("=")))
+#                 print(temp1.split("="))
+#             elif "<=" in y:
+#                 temp2=y.split(";")[0]
+#                 edges_set.append(set(temp2.split("<=")))
+#                 print(temp2.split("<="))
+#             elif "==" in y:
+#                 pass
+#             elif "." in y:
+#                 temp3=y.replace(".", '')
+#                 temp3=temp3.replace(" ", '')
+#                 temp3=temp3.replace(")", '')
+#                 if ":" in temp3:
+#                     temp3=temp3.split("[")[0]
+#                 else: temp3=temp3.split(";")[0]
+#                 # temp3=temp3.replace("", '')
+#                 edges_set.append(set(temp3.split("(")))
+#                 print(temp3.split("("))
+#             # edges_set.append(set([x,y]))
+#             # print (y)
+for x in variables2:
+    for y in variables2:
+        if (x!=y):
+            for m in body:
+                if x in m:
+                    if y in m:
+                        print(x,y)
+                        edges_set.append(set([x,y]))
+
+
+print (edges_set)
+
+G=nx.Graph()
+G.add_nodes_from(variables2)
+G.add_edges_from(edges_set)
+
+print(G.nodes())
+print(G.edges())
+
+print(type(G.nodes()))
+print(type(G.edges()))
+
+nx.draw(G)
+# plt.savefig("simple_path.png") # save as png
+plt.show()
